4f2781f276b8621ce22bb167d21f4e16d192dfd9
roksana35 <roksana7235@gmail.com>
2024-07-04 08:34:25 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h3>hello </h3>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Layout/Home.jsx
@@ -0,0 +1,20 @@
+import Banner from "../Components/Banner";
+import Navbar from "../Components/Navbar";
+
+
+const Home = () => {
+    return (
+        <div> 
+            <div>
+            <Navbar></Navbar>
+            </div>
+            <div>
+                <Banner></Banner>
+            </div>
+            
+        </div>
+        
+    );
+};
+
+export default Home;
